ed624835ab6feb3ac85b2541eb6b981e28cacc75
poko09 <71036264+poko09@users.noreply.github.com>
2022-01-17 21:43:32 +0100

    Delete Index directory

--- Index/result.js
+++
@@ -1,9 +0,0 @@
-function detector(parameter){
-    result = ""
-    if(parameter === 0){
-        result = "Hate speech detected";
-    }else if(parameter === 1){
-        result = "Text is neutral";
-    }
-    document.getElementById("decission").innerHTML = result;
-}
